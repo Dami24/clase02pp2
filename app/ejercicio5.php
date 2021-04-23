3d1c47d907f52d9d824683b1d69d04ce272b5e99
Dami24 <ojedadamian1@gmail.com>
2021-04-23 20:42:24 -0300

    se edito ejercicio5

--- app/ejercicio5.php
+++ app/ejercicio5.php
@@ -2,68 +2,32 @@
 
 $edad = rand(1,100);
 
-echo "La edad es: ".edad. "<br>";
+echo "La edad es: ".$edad. "<br>";
 
 switch ($edad) {
 	case 1:
-		echo "Es menor";
-		break;
 	case 2:
-		echo "Es menor";
-		break;
 	case 3:
-		echo "Es menor";
-		break;
 	case 4:
-		echo "Es menor";
-		break;
 	case 5:
-		echo "Es menor";
-		break;
 	case 6:
-		echo "Es menor";
-		break;
 	case 7:
-		echo "Es menor";
-		break;
 	case 8:
-		echo "Es menor";
-		break;
 	case 9:
-		echo "Es menor";
-		break;
 	case 10:
-		echo "Es menor";
-		break;
 	case 11:
-		echo "Es menor";
-		break;
 	case 12:
 		echo "Es menor";
 		break;
 	case 13:
-		echo "Es adolescente";
-		break;
 	case 14:
-		echo "Es adolescente";
-		break;
 	case 15:
-		echo "Es adolescente";
-		break;
 	case 16:
-		echo "Es adolescente";
-		break;
 	case 17:
 		echo "Es adolescente";
 		break;
-	case 18:
-		echo "Es mayor";
-		break;
-	case 19:
-		echo "Es mayor";
-		break;
-	case 20:
-		echo "Es mayor";
+	default:
+		echo "Es mayor de edad";
 		break;
 
 	
